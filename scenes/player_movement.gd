bcf6409e33f1eb7e05def7a47e5915f60eb011dd
GreyWolfEta <greymagicwolf@gmail.com>
2025-03-17 14:13:27 +0100

    click signal

--- scenes/player_movement.gd
+++ scenes/player_movement.gd
@@ -1,12 +1,15 @@
 extends Node
 
 
+var invenotry_opened = false
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and !invenotry_opened:
 		var dist = abs($Player.position - event.position)
 		if $Player.moving or (dist[0] > 300):
 			$Player.set_target(event.position)
 
-
 func _on_player_click(_event: InputEventMouseButton) -> void:
-	print("Open Inventory")
+	if !invenotry_opened:
+		print("Open Inventory")
+	invenotry_opened = !invenotry_opened
